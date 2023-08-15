6a0eaf51c312387dcd2b319cc09d1fc585cd9273
Matías Arno <amatiasarno@gmail.com>
2023-08-15 17:17:11 -0300

    Demo finished

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("HOla!");
+import getAPIData from "./model/model";
+
+const API_BASE_URL = new URL('https://rickandmortyapi.com/api');
+
+(async function main() {
+
+    const response = await getAPIData(API_BASE_URL);
+
+    console.log(response);
+})();
+
---
+++ src/model/model.ts
@@ -0,0 +1,25 @@
+import { Character } from "./types";
+
+function processObject(dataObj: any) {
+    const collection = dataObj.results as Character[];
+
+    const result = collection.map((character: Character) => {
+        const { name, species } = character;
+
+        return { name, species };
+    });
+
+    return result;
+} 
+
+async function getAPIData(url: URL) {
+    const response = await fetch(url.href + '/character');
+
+    const jsonObj = await response.json();  
+
+    const formattedResult = processObject(jsonObj);
+
+    return formattedResult;
+}
+
+export default getAPIData;
